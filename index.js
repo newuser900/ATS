bfa7ffc7581bf183250eacb3219a408af7ef4b63
Abhishek Bhagate <bhagatea@gmail.com>
2021-07-14 02:02:12 +0530

    updates index.js production serve

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+
 require("dotenv").config();
+
+module.exports = () => {
+	// call dotenv and it will return an Object with a parsed key
+	const env = dotenv.config().parsed;
+
+	// reduce it to a nice object, the same as before
+	const envKeys = Object.keys(env).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(env[next]);
+		return prev;
+	}, {});
+
+	return {
+		plugins: [new webpack.DefinePlugin(envKeys)],
+	};
+};
 
 const app = express();
 app.use(express.json());
@@ -24,8 +42,12 @@ const PORT = process.env.PORT || 10000;
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
-}
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+console.log(process.env.NODE_ENV);
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 app.use("/users", require("./routes/userRouter"));
