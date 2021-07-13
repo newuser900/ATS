f6bb1202ae1932565caf1754000f5fc7db48c4a7
Abhishek Bhagate <bhagatea@gmail.com>
2021-07-14 02:38:50 +0530

    moves express catchall route to the bottom

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ const path = require("path");
 
 require("dotenv").config();
 
-module.exports = () => {
-	// call dotenv and it will return an Object with a parsed key
-	const env = dotenv.config().parsed;
-
-	// reduce it to a nice object, the same as before
-	const envKeys = Object.keys(env).reduce((prev, next) => {
-		prev[`process.env.${next}`] = JSON.stringify(env[next]);
-		return prev;
-	}, {});
-
-	return {
-		plugins: [new webpack.DefinePlugin(envKeys)],
-	};
-};
-
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -38,6 +23,8 @@ mongoose.connect(
 	}
 );
 
+app.use("/users", require("./routes/userRouter"));
+
 const PORT = process.env.PORT || 10000;
 
 if (process.env.NODE_ENV === "production") {
@@ -47,7 +34,5 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
-console.log(process.env.NODE_ENV);
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
-app.use("/users", require("./routes/userRouter"));
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
